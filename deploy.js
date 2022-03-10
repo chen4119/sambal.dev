242901157215d4d2cf27677f6a054b21f6fa6d10
chen4119 <chen4119@hotmail.com>
2022-03-10 18:42:29 -0500

    update theme

--- deploy.js
+++ deploy.js
@@ -1,6 +1,3 @@
 const fs = require("fs");
-const shelljs = require("shelljs");
 
-const result  = shelljs.ls("./public/node_modules/prismjs/themes");
-console.log(result);
 fs.copyFileSync("./404.html", "./public/404.html");
